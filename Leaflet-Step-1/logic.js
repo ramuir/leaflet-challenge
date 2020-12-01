a840f3bbf57110072cff19fb7d1d2bd6e5e5677c
ramuir <65628879+ramuir@users.noreply.github.com>
2020-11-30 23:07:06 -0600

    Latest push with changes to raius and color...addon lengend not fully funtional

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -16,24 +16,26 @@ var myMap = L.map("mapid", {
   var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
 
   d3.json(url, function(response){
-      // for (var i = 0; i < response.length; i++){
-      
+
       function colorPick(n){
             var color = "";
             if (n < 10) {
-              color = "red";
+              color = "#7a0177";
             }
             else if (n < 30) {
-              color = "green";
+              color = "#BD0026";
             }
             else if (n < 50) {
-              color = "orange";
+              color = "#E31A1C";
             }
             else if (n < 70) {
-              color = "yellow";
+              color = "#FC4E2A";
             }
             else if (n < 90){
-                color = "brown"
+                color = "#FD8D3C"
+            }
+            else {
+              color = "#FEB24C"
             }
             return color;
         }
@@ -42,19 +44,21 @@ var myMap = L.map("mapid", {
             if (n === 0){
             return 1;
         }
-        return n ;
+        return 10**n/30000 ;
     }
 
 
         function styleChoice(feature){
-            console.log(feature.properties.mag);
+            // console.log(feature.properties.mag);
     return {
         radius: radius(feature.properties.mag),
         fillColor: colorPick(feature.geometry.coordinates[2]),
         color:  colorPick(feature.geometry.coordinates[2]),
-        // weight: 0.5,
+        weight: 0.5,
         opacity: 1,
-        fillOpacity: 0.5
+        fillOpacity: 0.5,
+        stroke: 1,
+      
     }
 }
 
@@ -68,4 +72,36 @@ var myMap = L.map("mapid", {
         }
       
     }).addTo(myMap);
+
+    function getColor(dep){
+      return dep >5 ? "#7a0177":
+      dep >4 ? "#BD0026":
+      dep >3 ?"#E31A1C":
+      dep >2 ?"#FC4E2A":
+      dep >1 ?"#FD8D3C":
+      dep >0 ?"#FEB24C":
+      "#FFFFFF"
+    }
+
+    var legend = L.control({
+      position: 'bottomright'});
+
+    legend.onAdd = function () {
+
+    var div = L.DomUtil.create('div', 'info legend');
+    // var labels = ['<strong>Depth By Color</strong>'];
+    var categories = [-10,10,30,50,70,90];
+    var colors = ["#7a0177","#BD0026","#E31A1C","#FC4E2A","#FD8D3C","#FEB24C"];
+
+    for (var i = 0; i < categories.length; i++) {
+//console.log(colorPick(categories))
+            div.innerHTML += '<i style="background:' +  colors[i] + '"></i>' 
+            + categories[i] + (categories[i+1] ? '&ndash;' + categories[i+1] + '<br>' : '+');
+ 
+        };
+        // div.innerHTML = labels.join('<br>');
+    return div;
+    };
+    legend.addTo(myMap);
+
 });
--- Leaflet-Step-1/practice2.js
+++ Leaflet-Step-1/practice2.js
@@ -3,8 +3,8 @@ var myMap = L.map("mapid", {
     zoom: 2
   });
   
-  // Adding a tile layer (the background map image) to our map
-  // We use the addTo method to add objects to our map
+//   // Adding a tile layer (the background map image) to our map
+//   // We use the addTo method to add objects to our map
  L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
@@ -12,22 +12,19 @@ var myMap = L.map("mapid", {
     accessToken: API_KEY
   }).addTo(myMap);
 
-
-  var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
-  
-  function colorPick(n){
+  function myColor(n){
     var color = "";
     if (n < 10) {
-      color = "lime-green";
+      color = "red";
     }
     else if (n < 30) {
-      color = "yellow-green";
+      color = "green";
     }
     else if (n < 50) {
       color = "orange";
     }
     else if (n < 70) {
-      color = "burnt orange";
+      color = "yellow";
     }
     else if (n < 90){
         color = "brown"
@@ -36,114 +33,158 @@ var myMap = L.map("mapid", {
 }
 
 function radius(n){
-  return n;
+    if (n === 0){
+    return 1;
+}
+return n ;
 }
 
-  d3.json(url, function(response){
-      // for (var i = 0; i < response.length; i++){
-        var geometry = response.features[0].geometry;
-        // console.log(geometry.coordinates[2]);
-        function style(feature){
-          console.log(geometry.coordinates[2])
-          return{
+// var myStyle = { 
+//         radius: radius(feature.properties.mag),
+//         fillColor: colorPick(feature.geometry.coordinates[2]),
+//         color:  colorPick(feature.geometry.coordinates[2]),
+//         weight: 0.5,
+//         opacity: 1,
+//         fillOpacity: 0.5,
+//         stroke: 1,
+// };
+  var geojson = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
+  
+  L.geoJSON(geojson).addTo(myMap);
+
+
+
+
+
+//   function colorPick(n){
+//     var color = "";
+//     if (n < 10) {
+//       color = "lime-green";
+//     }
+//     else if (n < 30) {
+//       color = "yellow-green";
+//     }
+//     else if (n < 50) {
+//       color = "orange";
+//     }
+//     else if (n < 70) {
+//       color = "burnt orange";
+//     }
+//     else if (n < 90){
+//         color = "brown"
+//     }
+//     return color;
+// }
+
+// function radius(n){
+//   return n;
+// }
+
+//   d3.json(url, function(response){
+//       // for (var i = 0; i < response.length; i++){
+//         var geometry = response.features[0].geometry;
+//         // console.log(geometry.coordinates[2]);
+//         function style(feature){
+//           console.log(geometry.coordinates[2])
+//           return{
         
-        radius: radius(feature.properties.mag),
-        fillColor: colorPick(geometry.coordinates[2]),//this should call from inside but only shows up with this
-        color: colorPick(geometry.coordinates[2]),
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-          };
-        }
-    // var geoJson = response.features[0].geometry;
-    // var long = geoJson.coordinates[0];
-    // var lat = geoJson.coordinates[1];
-    // var depth = geoJson.coordinates[2];
-    // var magnitude = response.features[0].properties.mag;
+//         radius: radius(feature.properties.mag),
+//         fillColor: colorPick(geometry.coordinates[2]),//this should call from inside but only shows up with this
+//         color: colorPick(geometry.coordinates[2]),
+//         weight: 1,
+//         opacity: 1,
+//         fillOpacity: 0.8
+//           };
+//         }
+//     // var geoJson = response.features[0].geometry;
+//     // var long = geoJson.coordinates[0];
+//     // var lat = geoJson.coordinates[1];
+//     // var depth = geoJson.coordinates[2];
+//     // var magnitude = response.features[0].properties.mag;
  
-        // function color(n){
-        //     var color = "";
-        //     if (n < 10) {
-        //       color = "lime";
-        //     }
-        //     else if (n< 30) {
-        //       color = "yellow";
-        //     }
-        //     else if (n < 50) {
-        //       color = "orange";
-        //     }
-        //     else if (n < 70) {
-        //       color = "orange";
-        //     }
-        //     else if (n < 90){
-        //         color = "brown"
-        //     }
-        //     return color;
-        // }
+//         // function color(n){
+//         //     var color = "";
+//         //     if (n < 10) {
+//         //       color = "lime";
+//         //     }
+//         //     else if (n< 30) {
+//         //       color = "yellow";
+//         //     }
+//         //     else if (n < 50) {
+//         //       color = "orange";
+//         //     }
+//         //     else if (n < 70) {
+//         //       color = "orange";
+//         //     }
+//         //     else if (n < 90){
+//         //         color = "brown"
+//         //     }
+//         //     return color;
+//         // }
 
        
 
-    // var geometry = response.features[0].geometry;
-    // var long = geometry.coordinates[0];
-    // var lat = geometry.coordinates[1];
-    // var depth = geometry.coordinates[2];
-    // var magnitude = response.features[0].properties.mag;
+//     // var geometry = response.features[0].geometry;
+//     // var long = geometry.coordinates[0];
+//     // var lat = geometry.coordinates[1];
+//     // var depth = geometry.coordinates[2];
+//     // var magnitude = response.features[0].properties.mag;
 
 
 
-// L.geoJson(response, {
-//   // We turn each feature into a circleMarker on the map.
-//   pointToLayer: function(feature, geometry) {
-//     console.log(feature.properties.mag);
-//     return L.circleMarker(geometry, {
-//       fillOpacity: 0.5,
-//       color: color(geometry.alt),
-//       radius: feature.properties.mag,
-//       weight: .5,
-//       fillColor: color(geometry.alt),
-//       // Adjust radius
+// // L.geoJson(response, {
+// //   // We turn each feature into a circleMarker on the map.
+// //   pointToLayer: function(feature, geometry) {
+// //     console.log(feature.properties.mag);
+// //     return L.circleMarker(geometry, {
+// //       fillOpacity: 0.5,
+// //       color: color(geometry.alt),
+// //       radius: feature.properties.mag,
+// //       weight: .5,
+// //       fillColor: color(geometry.alt),
+// //       // Adjust radius
       
-  //     "popupContent": "<h1>" + feature.properties.place + "</h1>" + "<hr> <h3>Magnitude: " + feature.properties.mag + "</h3>"
-  //     // .addTo(myMap).bindPopup("<h1>" + feature.properties.place + "</h1>" + "<hr> <h3>Magnitude: " + feature.properties.mag + "</h3>");//.addTo(myMap);
-  // })}
+//   //     "popupContent": "<h1>" + feature.properties.place + "</h1>" + "<hr> <h3>Magnitude: " + feature.properties.mag + "</h3>"
+//   //     // .addTo(myMap).bindPopup("<h1>" + feature.properties.place + "</h1>" + "<hr> <h3>Magnitude: " + feature.properties.mag + "</h3>");//.addTo(myMap);
+//   // })}
 
-  L.geoJSON(response, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng)
-    },
-    style:style,
-    onEachFeature : function(feature,layer){
-      layer.bindPopup("<h1>" + feature.properties.place + "</h1>" + "<hr> <h3>Magnitude: " + feature.properties.mag + "</h3>");//.addTo(myMap);
-    }
+//   L.geoJSON(response, {
+//     pointToLayer: function (feature, latlng) {
+//         return L.circleMarker(latlng)
+//     },
+//     style:style,
+//     onEachFeature : function(feature,layer){
+//       layer.bindPopup("<h1>" + feature.properties.place + "</h1>" + "<hr> <h3>Magnitude: " + feature.properties.mag + "</h3>");//.addTo(myMap);
+//     }
   
-}).addTo(myMap);//.bindPopup("<h1>" + feature.properties.place + "</h1>" + "<hr> <h3>Magnitude: " + feature.properties.mag + "</h3>");//.addTo(myMap);
+// }).addTo(myMap);//.bindPopup("<h1>" + feature.properties.place + "</h1>" + "<hr> <h3>Magnitude: " + feature.properties.mag + "</h3>");//.addTo(myMap);
 
 
 
 
-        // L.circle([lat,long], {
-        //     fillOpacity: 0.75,
-        //     color: "white",
-        //     fillColor: color(depth),
-        //     // Adjust radius
-        //     radius: magnitude
-        //   }).addTo(myMap);//.bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
+//         // L.circle([lat,long], {
+//         //     fillOpacity: 0.75,
+//         //     color: "white",
+//         //     fillColor: color(depth),
+//         //     // Adjust radius
+//         //     radius: magnitude
+//         //   }).addTo(myMap);//.bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
 
-    // var myLines = [{
-    //     "type": "Circle",
-    //     "coordinates": [lat, long]
-    // }];
+//     // var myLines = [{
+//     //     "type": "Circle",
+//     //     "coordinates": [lat, long]
+//     // }];
 
-    // var myStyle = {
-    //     "color": color(),
-    //     "weight": 2,
-    //     "opacity": 0.65
-    // };
+//     // var myStyle = {
+//     //     "color": color(),
+//     //     "weight": 2,
+//     //     "opacity": 0.65
+//     // };
 
 
-    // L.geoJSON(myLines, {
-    //     style: myStyle
-    // }).addTo(map);
-    // }
+//     // L.geoJSON(myLines, {
+//     //     style: myStyle
+//     // }).addTo(map);
+//     // }
    
-});
+// });
